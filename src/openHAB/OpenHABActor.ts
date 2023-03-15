0d0d7a89bb11ece65dce58c66c968f5ce593a65e
woutslabbinck <wout.slabbinck@ugent.be>
2023-03-15 17:42:25 +0100

    feat: 80% done of openhabactor

--- src/openHAB/OpenHABActor.ts
+++ src/openHAB/OpenHABActor.ts
@@ -1,5 +1,6 @@
 import {OpenHABClient} from "./OpenHABClient";
 import {OpenHABRDFTranslator} from "./OpenHABRDFTranslator";
+import {Quad} from "n3";
 
 /**
  * V1: monitor an OpenHAB platform for changes in the state of item
@@ -7,7 +8,7 @@ import {OpenHABRDFTranslator} from "./OpenHABRDFTranslator";
  * V2: monitor an OpenHAB platform for changes in the state of items
  */
 export class OpenHABActor {
-    private client : OpenHABClient;
+    private client: OpenHABClient;
     private translator: OpenHABRDFTranslator
 
     constructor(client: OpenHABClient, translator: OpenHABRDFTranslator) {
@@ -16,6 +17,38 @@ export class OpenHABActor {
     }
 
     public monitorItem(item: string) {
+        // no idea yet how calling this method results in actually being subscribed.
+        // Maybe by it being a stream?
 
     }
+
+    /**
+     * todo: error handling -> maybe return like a status of succession instead of void?
+     * @param quads
+     * @return {Promise<void>}
+     */
+    public async storeItem(quads: Quad[]): Promise<void> {
+        // extract identifier of item -> maybe add as parameter, remember as2
+        const id = ''
+        // RDF to item
+        const item = await this.translator.translateRDFToItem(quads, id)
+        await this.client.setItem(item)
+    }
+
+    public async retrieveItem(identifier: string): Promise<Quad[]> {
+        // maybe some parsing of identifier?
+        const item = await this.client.readItem(identifier)
+        // item to RDF
+        const quads = this.translator.translateItemToRDF(item)
+        return quads
+    }
 }
+
+/**
+ * Subgoals:
+ * (i) retrieve the state of an item: check
+ * (ii) map an item state object to an RDF representation of the said item: check
+ * (iii) store the state of an item: check
+ * (iv) map an RDF representation of the state of an item to an item state object: check
+ * (v) subscribe to an item.
+ */
